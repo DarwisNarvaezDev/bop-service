864c9aadfca669dd4e39fefc22509e807cddfe71
Darwis Narvaez <darwisnarvaezdev@gmail.com>
2024-08-06 21:26:19 -0300

    With a ribbon

--- db/data-source.ts
+++ db/data-source.ts
@@ -4,7 +4,7 @@ export const dataSourceOptions: DataSourceOptions = {
   type: 'sqlite',
   database: 'bop.sqlite',
   entities: ['dist/**/*.entity.js'],
-  migrations: ["dist/Init/*.js"],
+  migrations: ["dist/db/migrations/*.js"],
   synchronize: true
 }
 
